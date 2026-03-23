1a4384932e21bdcf1d0d9604c62377193c051cbb
Amirul Naim <amirul431@gmail.com>
2026-03-24 02:19:36 +0800

    fix date 2.0

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -30,7 +31,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->forget('url.intended');
 
-        return redirect()->route('dashboard');
+        return redirect()->route('app.dashboard', [
+            'date' => Carbon::now()->toDateString(),
+            'period' => 'day',
+        ]);
     }
 
     /**
